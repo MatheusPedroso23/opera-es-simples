fb3208991c1acd3326dd2874f3454865733c043c
Matheus Pedroso <125520928+MatheusPedroso23@users.noreply.github.com>
2023-02-15 22:19:21 -0300

    incluindo projeto inicial

---
+++ script.js
@@ -0,0 +1,26 @@
+
+       function subtrair () {
+        var s1 = document.getElementById("number1").value;
+        var s2 = document.getElementById("number2").value;
+        var resul = s1 - s2;
+        alert('o resultado é ' + resul);
+    }
+    
+    function Dividir () {
+        var d1 = document.getElementById("number1").value;
+        var d2 = document.getElementById("number2").value;
+        var resul = d1 / d2;
+        alert('o resultado é ' + resul);
+    }
+    function Multiplicar () {
+        var m1 = document.getElementById("number1").value;
+        var m2 = document.getElementById("number2").value;
+        var resul = m1 * m2;
+        alert('o resultado é ' + resul);
+    }
+    function Somar () {
+        var s1 = document.getElementById("number1").value;
+        var s2 = document.getElementById("number2").value;
+        var resul = parseInt(s1) + parseInt(s2);
+        alert('o resultado é ' + resul);
+    }
